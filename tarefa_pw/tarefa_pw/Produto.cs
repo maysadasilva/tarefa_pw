733353ec7b0d9de6b84b3b0568bfd21a6a55aa2d
maysadasilva <maysapedro.01@hotmail.com>
2023-03-23 13:32:19 -0300

    Adicionando os métodos get e set na classe produto.

--- tarefa_pw/tarefa_pw/Produto.cs
+++ tarefa_pw/tarefa_pw/Produto.cs
@@ -8,6 +8,47 @@ namespace tarefa_pw
 {
     public class Produto
     {
+        private int Id;
+        private decimal Valor;
+        private string Descricao;
+
+        public Produto(int id, decimal valor, string descricao)
+        {
+            Id = id;
+            Valor = valor;
+            Descricao = descricao;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public void SetValor(decimal valor)
+        {
+            Valor = valor;
+        }
+
+        public void SetDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public decimal getValor()
+        {
+            return Valor;
+        }
+
+        public string getDescricao()
+        {
+            return Descricao;
+        }
+
 
     }
 }
